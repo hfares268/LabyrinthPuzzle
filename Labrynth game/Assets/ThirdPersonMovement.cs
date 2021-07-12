ddffb637c3afe6678f84ab0260831cd883f7e487
hfares268 <hassan.fares@mail.citytech.cuny.edu>
2021-07-12 13:33:22 -0400

    ThirdPerson Controller almost there

---
+++ Labrynth game/Assets/ThirdPersonMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public CharacterController controller;
+
+    public float speed = 6f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            controller.Move(direction * speed * Time.deltaTime);
+        }
+      
+    }
+}
